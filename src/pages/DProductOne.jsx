aa9703fb00f74d7c5dbe3169fe3742e170d8d7a7
Mahnoor-Yameen <mahnooryameen321@gmail.com>
2024-05-28 15:28:12 +0500

    vv

--- src/pages/DProductOne.jsx
+++ src/pages/DProductOne.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect,useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { CartContext } from '../context/addtocart/context';
 import { Link } from 'react-router-dom'
@@ -87,105 +87,142 @@ export default function DProductOne() {
   // all products from ex, uni, ess 
   const ArrayToMatch = [
     // HAIR UNITS
-    {thumbnail:img15,
-      title:"Hair Unit 7",
-      sign: "250"    
-    },{thumbnail:img16,
-      title:"Hair Unit 8",
-      sign: "170"    
-    },{thumbnail:img17,
-      title:"Hair Unit 9",
-      sign: "180"    
-    },{thumbnail:img18,
-      title:"Hair Unit 10",
-      sign: "250"    
-    },{thumbnail:img19,
-      title:"Hair Unit 11",
-      sign: "160"    
-    },{thumbnail:img20,
-      title:"Hair Unit 12",
-      sign: "180"    
-    },{thumbnail:img21,
-      title:"Hair Unit 13",
-      sign: "350"    
-    },{thumbnail:img13,
-      title:"Hair Unit 2",
-      sign: "300"    
+    {
+      thumbnail: img15,
+      title: "Hair Unit 7",
+      sign: "250"
+    }, {
+      thumbnail: img16,
+      title: "Hair Unit 8",
+      sign: "170"
+    }, {
+      thumbnail: img17,
+      title: "Hair Unit 9",
+      sign: "180"
+    }, {
+      thumbnail: img18,
+      title: "Hair Unit 10",
+      sign: "250"
+    }, {
+      thumbnail: img19,
+      title: "Hair Unit 11",
+      sign: "160"
+    }, {
+      thumbnail: img20,
+      title: "Hair Unit 12",
+      sign: "180"
+    }, {
+      thumbnail: img21,
+      title: "Hair Unit 13",
+      sign: "350"
+    }, {
+      thumbnail: img13,
+      title: "Hair Unit 2",
+      sign: "300"
     },
-    
-{thumbnail:img12,
-title:"Hair Unit 3",
-sign: "280"    
-},
- 
-  {thumbnail:img2,
-  title:"WIG 000180-02",
-  sign: "180"   } ,
 
-  // new additions:
+    {
+      thumbnail: img12,
+      title: "Hair Unit 3",
+      sign: "280"
+    },
 
-  {thumbnail:imgn1,
-    title:"Hair Unit 14",
-    sign: "180"   } ,
-    {thumbnail:imgn2,
-      title:"Hair Unit 15",
-      sign: "220"   } ,
-      {thumbnail:imgn3,
-        title:"Hair Unit 16",
-        sign: "180"   } ,
-        {thumbnail:imgn4,
-          title:"Hair Unit 17",
-          sign: "250"   } ,
-          {thumbnail:imgn5,
-            title:"Hair Unit 18",
-            sign: "160"   } ,
-            {thumbnail:imgn6,
-              title:"Hair Unit 19",
-              sign: "250"   } ,
-              {thumbnail:imgn7,
-                title:"Hair Unit 20",
-                sign: "180"   } ,
-                {thumbnail:imgn8,
-                  title:"Hair Unit 21",
-                  sign: "280"   } ,
-                  {thumbnail:imgn9,
-                    title:"Hair Unit 22",
-                     sign: "220"   } 
+    {
+      thumbnail: img2,
+      title: "WIG 000180-02",
+      sign: "180"
+    },
 
-                    // new addition ended
-,{thumbnail:img4,
-  title:"WIG 000298-01",
-  sign: "298"    
-},{thumbnail:img5,
-  title:"WIG 000277-01",
-  sign: "277"    
-},{thumbnail:img6,
-  title:"WIG 000268-01",
-  sign: "268"    
-},{thumbnail:img7,
-  title:"WIG 000278-01",
-  sign: "278"    
-},
-// {thumbnail:img8,
-//     title:"WIG 000180-01",
-//     sign: "180"    
-// }
-,{thumbnail:img14,
-  title:"Hair Unit 1",
-  sign: "355"    
-}
+    // new additions:
 
-,{thumbnail:img11,
-title:"Hair Unit 4",
-sign: "285"    
-},{thumbnail:img10,
-title:"Hair Unit 5",
-sign: "385"    
-},{thumbnail:img9,
-title:"Hair Unit 6",
-sign: "385"    
-},
-  
+    {
+      thumbnail: imgn1,
+      title: "Hair Unit 14",
+      sign: "180"
+    },
+    {
+      thumbnail: imgn2,
+      title: "Hair Unit 15",
+      sign: "220"
+    },
+    {
+      thumbnail: imgn3,
+      title: "Hair Unit 16",
+      sign: "180"
+    },
+    {
+      thumbnail: imgn4,
+      title: "Hair Unit 17",
+      sign: "250"
+    },
+    {
+      thumbnail: imgn5,
+      title: "Hair Unit 18",
+      sign: "160"
+    },
+    {
+      thumbnail: imgn6,
+      title: "Hair Unit 19",
+      sign: "250"
+    },
+    {
+      thumbnail: imgn7,
+      title: "Hair Unit 20",
+      sign: "180"
+    },
+    {
+      thumbnail: imgn8,
+      title: "Hair Unit 21",
+      sign: "280"
+    },
+    {
+      thumbnail: imgn9,
+      title: "Hair Unit 22",
+      sign: "220"
+    }
+
+    // new addition ended
+    , {
+      thumbnail: img4,
+      title: "WIG 000298-01",
+      sign: "298"
+    }, {
+      thumbnail: img5,
+      title: "WIG 000277-01",
+      sign: "277"
+    }, {
+      thumbnail: img6,
+      title: "WIG 000268-01",
+      sign: "268"
+    }, {
+      thumbnail: img7,
+      title: "WIG 000278-01",
+      sign: "278"
+    },
+    // {thumbnail:img8,
+    //     title:"WIG 000180-01",
+    //     sign: "180"    
+    // }
+    , {
+      thumbnail: img14,
+      title: "Hair Unit 1",
+      sign: "355"
+    }
+
+    , {
+      thumbnail: img11,
+      title: "Hair Unit 4",
+      sign: "285"
+    }, {
+      thumbnail: img10,
+      title: "Hair Unit 5",
+      sign: "385"
+    }, {
+      thumbnail: img9,
+      title: "Hair Unit 6",
+      sign: "385"
+    },
+
     // Extensions
     {
       thumbnail: img1EX,
@@ -314,7 +351,7 @@ sign: "385"
   ]
 
   useEffect(() => {
-    const Found = ArrayToMatch.find(item => item.title === hollowtwo)
+    const Found = ArrayToMatch?.find(item => item?.title === hollowtwo)
 
     if (Found) {
       console.log("Found:", Found);
@@ -331,7 +368,7 @@ sign: "385"
 
 
   const [ProductQuantity, setProductQuantity] = useState(1)
-  const {state,dispatch}=useContext(CartContext)
+  const { state, dispatch } = useContext(CartContext)
 
   const addtocart = () => {
 
@@ -340,10 +377,10 @@ sign: "385"
       ProductQuantity,
       TotalPrice: productdetail?.sign * ProductQuantity
     };
-  
+
     // Check if the product is already in the cart
     const existingProductIndex = state?.cart?.findIndex(item => item?.title === productdetail?.title);
-  
+
     if (existingProductIndex !== -1) {
       // If the product is already in the cart, update its quantity
       const updatedCart = [...state.cart];
@@ -366,56 +403,56 @@ sign: "385"
   return (
     <>
 
-<div>
-      {/* left right */}
-      <div className='row container py-5' >
+      <div>
+        {/* left right */}
+        <div className='row container py-5' >
 
-        {/* left */}
-        <div className=" col-md-6 col-sm-12">
-          {
-            productdetail?.thumbnail && <ImageSection images={productdetail?.thumbnail} />
-          }
-        </div>
-
-        {/* right */}
-        {/* ADD TO CART FUNCTIONALITY */}
-        <div className=' col-md-6 col-sm-12'>
-
-          {/* intro */}
-          <div className='my-5'>
-            <h1>{productdetail?.title}</h1>
-            <p className='' style={{fontSize:"18px"}}>Price:  &#163; {productdetail?.sign}</p>
-            <p className='text-center my-5' style={{ fontSize: "18px" }}>Tax included. Shipping calculated at checkout.</p>
+          {/* left */}
+          <div className=" col-md-6 col-sm-12">
+            {
+              productdetail?.thumbnail && <ImageSection images={productdetail?.thumbnail} />
+            }
           </div>
 
-{/* 6 col ki space pe working kr rhy */}
+          {/* right */}
+          {/* ADD TO CART FUNCTIONALITY */}
+          <div className=' col-md-6 col-sm-12'>
+
+            {/* intro */}
+            <div className='my-5'>
+              <h1>{productdetail?.title}</h1>
+              <p className='' style={{ fontSize: "18px" }}>Price:  &#163; {productdetail?.sign}</p>
+              <p className='text-center my-5' style={{ fontSize: "18px" }}>Tax included. Shipping calculated at checkout.</p>
+            </div>
+
+            {/* 6 col ki space pe working kr rhy */}
 
 
 
-          {/* quantity */}
+            {/* quantity */}
             <div className='d-flex justify-content-center my-2 align-items-center '>
-              <span className='mx-5 ' style={{fontSize:"15px", fontWeight:"bold"}}>Quantity:</span>
+              <span className='mx-5 ' style={{ fontSize: "15px", fontWeight: "bold" }}>Quantity:</span>
               <button disabled={ProductQuantity > 1 ? false : true} className="btn btn-dark border  py-2" onClick={() => setProductQuantity(ProductQuantity - 1)}>-</button>
               <span className='mx-3'>{ProductQuantity}</span>
               <button className="btn btn-dark border mx-3 py-2" onClick={() => setProductQuantity(ProductQuantity + 1)}>+</button>
             </div >
 
-{/* 6 cols again */}
-          <div className='d-flex my-5'>
-            {/* <button className='p-4 mx-3 bg-light border' onClick={addtocart}><Link to="/checkout" className='text-dark  text-decoration-none'>Other Payment Options</Link></button> */}
-            <button className=' p-4 mx-3 btn btn-dark border' onClick={addtocart}>Add to Cart</button>
+            {/* 6 cols again */}
+            <div className='d-flex my-5'>
+              {/* <button className='p-4 mx-3 bg-light border' onClick={addtocart}><Link to="/checkout" className='text-dark  text-decoration-none'>Other Payment Options</Link></button> */}
+              <button className=' p-4 mx-3 btn btn-dark border' onClick={addtocart}>Add to Cart</button>
+
+            </div>
+
+
+
+
+
+
 
           </div>
-
-          
-
-
-
-
-
         </div>
       </div>
-</div>
 
     </>
   )
